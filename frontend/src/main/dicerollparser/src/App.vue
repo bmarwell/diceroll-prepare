f4a3c7bdb677246e0cef3550160a6906c6d9d850
Benjamin Marwell <bmarwell@apache.org>
2023-04-26 11:37:07 +0200

    add logo

--- frontend/src/main/dicerollparser/src/App.vue
+++ frontend/src/main/dicerollparser/src/App.vue
@@ -33,23 +33,53 @@ const clearForms = () => {
   rollResult.value = "";
   rollDetails.value = "";
 };
+
+const dice = [
+  "dice-1",
+  "dice-1-outline",
+  "dice-2",
+  "dice-2-outline",
+  "dice-3",
+  "dice-3-outline",
+  "dice-4",
+  "dice-4-outline",
+  "dice-5",
+  "dice-5-outline",
+  "dice-6",
+  "dice-6-outline",
+  "dice-d4",
+  "dice-d4-outline",
+  "dice-d6",
+  "dice-d6-outline",
+  "dice-d8",
+  "dice-d8-outline",
+  "dice-d10",
+  "dice-d10-outline",
+  "dice-d12",
+  "dice-d12-outline",
+  "dice-d20",
+  "dice-d20-outline",
+  "dice-multiple",
+  "dice-multiple-outline",
+];
+
+const getIcon = (): string => {
+  const idx = Math.floor(Math.random() * dice.length);
+  return dice[idx];
+};
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <h1>
+      <v-icon>{{ `mdi-${getIcon()}` }}</v-icon> Dice roll parser
+    </h1>
   </header>
 
   <main>
-    <v-container class=".w-auto">
-      <v-row no-gutters class=".w-auto">
-        <v-col sm="6">
+    <v-container class="">
+      <v-row no-gutters class="">
+        <v-col md="6" cols="12">
           <v-card title="Action" class="ma-2 pa-2">
             <template #actions>
               <v-btn prepend-icon="mdi-dice-5" @click="onRollDice">Roll!</v-btn>
@@ -64,7 +94,7 @@ const clearForms = () => {
           ></RollOptions>
           <DiceSelector ref="diceSelector" v-model="diceGroups"></DiceSelector>
         </v-col>
-        <v-col sm="6">
+        <v-col md="6" cols="12">
           <v-card title="Result" class="ma-2 pa-2">
             <v-text-field v-model="rollResult" readonly class="monospaced">
               <template #append-inner>
@@ -102,6 +132,7 @@ const clearForms = () => {
 <style scoped>
 header {
   line-height: 1.5;
+  place-items: center;
 }
 
 .monospaced {
@@ -109,25 +140,26 @@ header {
   font-size: 0.6em;
 }
 
-.logo {
+header h1 {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 1rem auto 1rem;
+  text-align: center;
 }
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
+    display: block;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
+  h1 {
     margin: 0 2rem 0 0;
+    text-align: center;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
+  header h1 {
+    display: block;
     flex-wrap: wrap;
   }
 }
